8c92cc5bb7f34de910926ca82ed5eca8e91e5eb2
muborakxon <toychiyevamuborebonu@gmail.com>
2023-06-09 01:02:03 +0500

    Implemented custom exceptions

---
+++ src/main/java/org/itpu/exception/CsvParserException.java
@@ -0,0 +1,9 @@
+package org.itpu.exception;
+
+public class CsvParserException extends RuntimeException{
+
+    public CsvParserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
